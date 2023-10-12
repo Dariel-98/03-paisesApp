4f6cf5e80acfb90e3b5e1930529f82d472bec26b
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-10-12 15:40:05 -0400

    Servicio para buscar paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    PaisModule, 
+    SharedModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
