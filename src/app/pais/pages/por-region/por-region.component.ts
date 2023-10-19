6dbc822f7c4e7bbbd55b8246081ed02555a2c4ea
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-10-19 14:33:57 -0400

    Clases de CSS condicionales

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -4,8 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
 export class PorRegionComponent {
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC',
+  ];
+  regionActiva: string = '';
 
+  constructor() {}
+
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
